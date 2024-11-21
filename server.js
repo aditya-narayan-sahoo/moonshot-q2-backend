bf2aa2c35e8ba310049ba28f9b6c9912c94c597d
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-21 19:41:50 +0530

    Misc Update

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const { errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const allowedOrigins = ["http://localhost:3000"];
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,

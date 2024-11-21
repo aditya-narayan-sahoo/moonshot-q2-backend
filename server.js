ef7bb72e8232a2339c35bdd6cce2cf9d68c1e240
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-21 17:41:27 +0530

    Allow CORS from localhost

--- server.js
+++ server.js
@@ -11,9 +11,21 @@ const chartRoutes = require("./routes/chartRoutes");
 const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
+const allowedOrigins = ["http://localhost:3000"];
 
 //Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler);

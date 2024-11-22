97999a6471326f091c14ef159d75ff4ba274381a
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-22 17:52:00 +0530

    Remove reduntant console log

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const allowedOrigins = [
   "https://moonshot-q2-frontend.vercel.app",
 ];
 
-console.log("MONGO_URI:", process.env.MONGO_URI);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB Atlas"))
